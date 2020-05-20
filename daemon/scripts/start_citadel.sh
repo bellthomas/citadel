a1f0a060e7ee5ef83c7f02e638b8d1aa6316c518
Alexander Bell-Thomas <ahb36@cl.cam.ac.uk>
2020-05-20 13:31:26 +0100

    Fix systemd priorities, and return an error when enclave fails to start

--- daemon/scripts/start_citadel.sh
+++ daemon/scripts/start_citadel.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 source /opt/intel/sgxsdk/environment
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TIME=$(date +%s)
 
-#$DIR/citadel $@
+# Sort logs.
+mkdir -p $DIR/logs
+[ -f $DIR/logs/citadel.log ] && mv $DIR/logs/citadel.log $DIR/logs/citadel.$TIME
+touch $DIR/logs/citadel.log
+
+# $DIR/citadel $@ | tee $DIR/logs/citadel.log
+$DIR/citadel $@ > $DIR/logs/citadel.log
